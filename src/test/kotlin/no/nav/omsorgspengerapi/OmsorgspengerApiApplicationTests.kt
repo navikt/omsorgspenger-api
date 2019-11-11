bd8fb1f9d751ef7fd1bf67fa1369620745f168d7
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2019-11-11 12:31:08 +0100

    Initial commit

---
+++ src/test/kotlin/no/nav/omsorgspengerapi/OmsorgspengerApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.nav.omsorgspengerapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OmsorgspengerApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
