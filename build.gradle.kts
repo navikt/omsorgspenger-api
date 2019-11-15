3d2810d75a1f17c79efbe4bae1372b3bd7c68d8f
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2019-11-15 13:27:02 +0100

    Validates audience on token

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.2.1.RELEASE"
-	id("io.spring.dependency-management") version "1.0.8.RELEASE"
-	kotlin("jvm") version "1.3.50"
-	kotlin("plugin.spring") version "1.3.50"
+    id("org.springframework.boot") version "2.2.1.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    kotlin("jvm") version "1.3.50"
+    kotlin("plugin.spring") version "1.3.50"
 }
 
 group = "no.nav"
@@ -12,55 +12,54 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	val springSecurityVersion = "5.2.1.RELEASE"
+    val springSecurityVersion = "5.2.1.RELEASE"
 
-	// See here for more info about configuring security: https://docs.spring.io/spring-security/site/docs/current/reference/html/webflux-oauth2.html#webflux-oauth2-resource-server
-	// https://mvnrepository.com/artifact/org.springframework.security/spring-security-oauth2-resource-server
-	implementation("org.springframework.security:spring-security-oauth2-resource-server:$springSecurityVersion")
+    // See here for more info about configuring security: https://docs.spring.io/spring-security/site/docs/current/reference/html/webflux-oauth2.html#webflux-oauth2-resource-server
+    // https://mvnrepository.com/artifact/org.springframework.security/spring-security-oauth2-resource-server
+    implementation("org.springframework.security:spring-security-oauth2-resource-server:$springSecurityVersion")
 
-	// https://mvnrepository.com/artifact/org.springframework.security/spring-security-oauth2-jose
-	implementation("org.springframework.security:spring-security-oauth2-jose:$springSecurityVersion")
-
+    // https://mvnrepository.com/artifact/org.springframework.security/spring-security-oauth2-jose
+    implementation("org.springframework.security:spring-security-oauth2-jose:$springSecurityVersion")
 
     // https://mvnrepository.com/artifact/org.springframework.security/spring-security-config
     implementation("org.springframework.security:spring-security-config:$springSecurityVersion")
 
-	// https://mvnrepository.com/artifact/net.logstash.logback/logstash-logback-encoder
-	implementation("net.logstash.logback:logstash-logback-encoder:6.2")
-	implementation("io.micrometer:micrometer-core:1.3.1")
-	implementation( "io.micrometer:micrometer-registry-prometheus:1.3.1")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-	}
-	testImplementation("io.projectreactor:reactor-test")
-	testImplementation("org.springframework.security:spring-security-test")
+    // https://mvnrepository.com/artifact/net.logstash.logback/logstash-logback-encoder
+    implementation("net.logstash.logback:logstash-logback-encoder:6.2")
+    implementation("io.micrometer:micrometer-core:1.3.1")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.3.1")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
 }
--- src/main/kotlin/no/nav/omsorgspengerapi/SecurityConfig.kt
+++ src/main/kotlin/no/nav/omsorgspengerapi/SecurityConfig.kt
@@ -1,13 +1,27 @@
 package no.nav.omsorgspengerapi
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
 import org.springframework.security.web.server.SecurityWebFilterChain
 
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(
+        @Qualifier("jwtDecoder") private val reactiveJwtDecoder: ReactiveJwtDecoder,
+        private val audienceValidator: AudienceValidator
+) {
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(AudienceValidator::class.java)
+    }
 
     @Bean
     internal fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
@@ -17,8 +31,15 @@ class SecurityConfig {
                             .pathMatchers("/actuator/**").permitAll()
                             .anyExchange().authenticated()
                 }
-                .oauth2ResourceServer { it.jwt() }
+                .oauth2ResourceServer().jwt().jwtDecoder(jwtDecoder())
         return http.build()
     }
 
+
+    fun jwtDecoder(): ReactiveJwtDecoder {
+        val tokenAudienceValidator: DelegatingOAuth2TokenValidator<Jwt> = DelegatingOAuth2TokenValidator(audienceValidator)
+        reactiveJwtDecoder as NimbusReactiveJwtDecoder
+        reactiveJwtDecoder.setJwtValidator(tokenAudienceValidator)
+        return reactiveJwtDecoder
+    }
 }
