b64152f2a946f533388dbf07a3982e139068fd46
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2019-12-02 13:50:57 +0100

    Configures reactorHttpConnector

--- src/main/kotlin/no/nav/omsorgspengerapi/config/general/K9LookupClient.kt
+++ src/main/kotlin/no/nav/omsorgspengerapi/config/general/K9LookupClient.kt
@@ -4,13 +4,15 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.client.reactive.ClientHttpConnector
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.security.oauth2.server.resource.web.reactive.function.client.ServerBearerExchangeFilterFunction
 import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.ExchangeFunction
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import reactor.netty.tcp.ProxyProvider
+import reactor.netty.tcp.TcpClient
 
 
 @Configuration
@@ -18,6 +20,23 @@ class K9LookupClient {
 
     companion object {
         private val log: Logger = LoggerFactory.getLogger(K9LookupClient.javaClass)
+    }
+
+    @Bean
+    fun httpClient(): HttpClient {
+        return HttpClient.create()
+                .tcpConfiguration { tcpClient: TcpClient ->
+                    tcpClient.proxy { proxy: ProxyProvider.TypeSpec ->
+                        proxy.type(ProxyProvider.Proxy.HTTP)
+                                .host("http://webproxy-utvikler.nav.no")
+                                .port(8088)
+                    }
+                }
+    }
+
+    @Bean
+    fun reactorClientHttpConnector(httpClient: HttpClient): ReactorClientHttpConnector? {
+        return ReactorClientHttpConnector(httpClient)
     }
 
     @Bean()
