f70f4f4b6289dd97d73ca77dc833c99ecbe29238
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2019-12-04 09:22:39 +0100

    adjusting lookup query

--- src/main/kotlin/no/nav/omsorgspengerapi/barn/lookup/ChildLookupService.kt
+++ src/main/kotlin/no/nav/omsorgspengerapi/barn/lookup/ChildLookupService.kt
@@ -7,7 +7,9 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.util.UriBuilder
 import reactor.core.publisher.Flux
+import java.net.URI
 
 @Service
 class ChildLookupService(
@@ -16,24 +18,31 @@ class ChildLookupService(
         private val tracer: Tracer) {
 
     @Value("\${nav.no.gateways.k9_lookup_url}")
-    lateinit var baseUrl: String
+    lateinit var baseUrl: URI
 
     companion object {
         private val logger: Logger = LoggerFactory.getLogger(ChildLookupService::class.java)
-        private val attributter = Pair("a", listOf("barn[].aktør_id",
+        private val attributes = listOf(
+                "barn[].aktør_id",
                 "barn[].fornavn",
                 "barn[].mellomnavn",
                 "barn[].etternavn",
                 "barn[].fødselsdato")
-        )
+
     }
 
     fun lookupChild(): Flux<ChildLookupDTO> {
 
         return client
                 .get()
-                .uri(baseUrl)
-                .attributes { attributter }
+                .uri {uri: UriBuilder -> uri
+                        .scheme(baseUrl.scheme)
+                        .host(baseUrl.host)
+                        .port(baseUrl.port)
+                        .path(baseUrl.path)
+                        .queryParam("a", attributes)
+                        .build()
+                }
                 .header("X-Correlation-ID", tracer.currentSpan().context().traceIdString())
                 .header(apiGatewayApiKey.header, apiGatewayApiKey.key)
                 .retrieve()
