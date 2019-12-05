59d7fcc35949f7911a699ea96f02490b68eaf9f6
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2019-12-05 10:34:51 +0100

    Disabling nonProxyHosts on webClient

--- src/main/kotlin/no/nav/omsorgspengerapi/TestController.kt
+++ src/main/kotlin/no/nav/omsorgspengerapi/TestController.kt
@@ -1,14 +1,20 @@
 package no.nav.omsorgspengerapi
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 
 @RestController
-class TestController {
+class TestController(private val webClient: WebClient) {
 
     @GetMapping("/unprotected")
-    fun unprotected(): Mono<String> {
-        return Mono.just("UnProtected")
+    fun unprotected(): Mono<ResponseEntity<Void>> {
+        return webClient.get()
+                .uri("https://www.vg.no")
+                .retrieve()
+                .toBodilessEntity()
+
     }
 }
