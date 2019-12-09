cd362c82e91e5b9c24c55e823b6727bf49d5eb04
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2019-12-09 10:18:05 +0100

    Configures webFlux httpMessageCodes. This enables setting property values on jackson from properties/yaml

---
+++ src/main/kotlin/no/nav/omsorgspengerapi/config/WebFluxConfiguration.kt
@@ -0,0 +1,25 @@
+package no.nav.omsorgspengerapi.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.codec.ServerCodecConfigurer
+import org.springframework.http.codec.json.Jackson2JsonDecoder
+import org.springframework.http.codec.json.Jackson2JsonEncoder
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+
+@Configuration
+@EnableWebFlux
+class WebFluxConfiguration(private val objectMapper: ObjectMapper): WebFluxConfigurer {
+
+    override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
+        configurer.defaultCodecs().jackson2JsonEncoder(
+                Jackson2JsonEncoder(objectMapper)
+        )
+        configurer.defaultCodecs().jackson2JsonDecoder(
+                Jackson2JsonDecoder(objectMapper)
+        )
+    }
+
+}
