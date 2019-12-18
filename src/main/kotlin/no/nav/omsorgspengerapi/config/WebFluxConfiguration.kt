e21c40f09be866bce10ea8fe4928b78b1795e145
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2019-12-18 12:48:36 +0100

    Configures max-in-memory limit on server codec.

--- src/main/kotlin/no/nav/omsorgspengerapi/config/WebFluxConfiguration.kt
+++ src/main/kotlin/no/nav/omsorgspengerapi/config/WebFluxConfiguration.kt
@@ -2,6 +2,7 @@ package no.nav.omsorgspengerapi.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import no.nav.omsorgspengerapi.common.OmsorgspengerAPIErrorErrorAttributes
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.reactive.error.ErrorAttributes
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,11 +11,15 @@ import org.springframework.http.codec.json.Jackson2JsonDecoder
 import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
+import java.net.URI
 
 
 @Configuration
 @EnableWebFlux
 class WebFluxConfiguration(private val objectMapper: ObjectMapper): WebFluxConfigurer {
+
+    @Value("\${spring.codec.max-in-memory-size}")
+    lateinit var maxInMemoryLimit: Integer
 
     override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
         configurer.defaultCodecs().jackson2JsonEncoder(
@@ -23,6 +28,7 @@ class WebFluxConfiguration(private val objectMapper: ObjectMapper): WebFluxConfi
         configurer.defaultCodecs().jackson2JsonDecoder(
                 Jackson2JsonDecoder(objectMapper)
         )
+        configurer.defaultCodecs().maxInMemorySize(maxInMemoryLimit.toInt())
     }
 
     @Bean
