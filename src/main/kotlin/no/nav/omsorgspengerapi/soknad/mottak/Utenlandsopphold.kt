a4db27acdaed6eecbd218dd9554901efd7d88bc5
Jan Tore Bøe <jan.tore.boe@nav.no>
2020-01-08 11:51:43 +0100

    Feil format på dato

--- src/main/kotlin/no/nav/omsorgspengerapi/soknad/mottak/Utenlandsopphold.kt
+++ src/main/kotlin/no/nav/omsorgspengerapi/soknad/mottak/Utenlandsopphold.kt
@@ -1,10 +1,11 @@
 package no.nav.omsorgspengerapi.soknad.mottak
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
 
 data class Utenlandsopphold(
-        val fraOgMed: LocalDate,
-        val tilOgMed: LocalDate,
+        @JsonFormat(pattern = "yyyy-MM-dd") val fraOgMed: LocalDate,
+        @JsonFormat(pattern = "yyyy-MM-dd") val tilOgMed: LocalDate,
         val landkode: String,
         val landnavn: String
 ) {
