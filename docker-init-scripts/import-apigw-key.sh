33d80e9cb85f726c1fe5d786071995ae999c108f
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2020-02-07 15:03:19 +0100

    Fikser path til apiKey.

--- docker-init-scripts/import-apigw-key.sh
+++ docker-init-scripts/import-apigw-key.sh
@@ -3,7 +3,7 @@
 echo "Importing API-GW Key"
 
 KEY="API_GATEWAY_API_KEY"
-VALUE=$(cat x-nav-apiKey)
+VALUE=$(cat /var/run/secrets/nais.io/apigw/x-nav-apiKey)
 
 echo "- exporting API_GATEWAY_API_KEY"
 export "$KEY"="$VALUE"
