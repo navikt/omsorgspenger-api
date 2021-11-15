3fef8b9f1cdc454721cfee05e2dbfbf8af2c9674
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-11-15 09:36:10 +0100

    Fjerner ubrukt endepunkt url.

--- src/main/kotlin/no/nav/omsorgspenger/felles/Endepunkter.kt
+++ src/main/kotlin/no/nav/omsorgspenger/felles/Endepunkter.kt
@@ -3,7 +3,6 @@ package no.nav.omsorgspenger.felles
 const val SØKNAD_URL = "/soknad"
 const val VALIDERING_URL = "/soknad/valider"
 const val SØKER_URL = "/soker"
-const val SØKER_ETTERSENDING_URL = "/ettersending/soker"
 const val BARN_URL = "/barn"
 const val VEDLEGGID_URL = "/{vedleggId}"
 const val VEDLEGG_URL = "/vedlegg"
